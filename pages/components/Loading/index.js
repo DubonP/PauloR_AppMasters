914360ed95acde2cdccd9b496a615e9b95e32aed
PauloRossi <paulogustavorossi@gmail.com>
2022-07-31 16:29:23 -0300

    retirando imports

--- pages/components/Loading/index.js
+++ pages/components/Loading/index.js
@@ -1,5 +1,3 @@
-import { useState, CSSProperties } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
 import styles from "../../../styles/Loading.module.css";
 
 export const Loading = () => {
