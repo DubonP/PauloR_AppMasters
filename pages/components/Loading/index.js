82abffbdb81f3c34d56562fbbbcabfa36418dbf9
PauloRossi <paulogustavorossi@gmail.com>
2022-07-31 16:39:34 -0300

    tentando funcionar no vercel

--- pages/components/Loading/index.js
+++ pages/components/Loading/index.js
@@ -3,7 +3,7 @@ import styles from "../../../styles/Loading.module.css";
 export const Loading = () => {
   return (
     <div className={styles.Loading}>
-      <img src="Spinner.svg" alt="loading" />
+      <image src="Spinner.svg" alt="loading" />
     </div>
   );
 };
