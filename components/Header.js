d3cd2765aaabfafc4b3f77655523c82a6975a2b3
PauloRossi <paulogustavorossi@gmail.com>
2022-08-04 17:44:25 -0300

    header design e responsivo

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,29 @@
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import instituicoes from "../pages/instituicoes.js";
+import Image from "next/image";
 
 export const Header = () => {
   return (
-    <div className={styles.All_header}>
-      <header>
-        <h1>Doação de computadores</h1>
-      </header>
-      <div className={styles.Buttons}>
+    <header className={styles.All_header}>
+      <div className={styles.header_logo}>
+        <div className={styles.header_logo_img}>
+          <Image src="/images/heart.png" layout="fill" alt="Background" />
+        </div>
         <Link href="/">
-          <button className={styles.Button}>Home</button>
-        </Link>
-        <Link href="/instituicoes">
-          <button href="/" className={styles.Button}>
-            instituições
-          </button>
+          <h1>Doação de computadores</h1>
         </Link>
       </div>
-    </div>
+      <div className={styles.Buttons}>
+        <Link href="/">
+          <span className={styles.Button}>Home</span>
+        </Link>
+        <Link href="/instituicoes">
+          <span href="/" className={styles.Button}>
+            instituições
+          </span>
+        </Link>
+      </div>
+    </header>
   );
 };
